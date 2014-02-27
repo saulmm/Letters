a16b341a7ab363892d632c6996c4eea914b6c6fa
Saúl <saulmm2@gmail.com>
2014-02-27 11:21:26 +0100

    Improvements with the color transitions :D

--- app/src/main/java/edu/saulmm/letters/Utils.java
+++ app/src/main/java/edu/saulmm/letters/Utils.java
@@ -4,7 +4,13 @@ package edu.saulmm.letters;
 public class Utils {
     public static final int STROKE_COLOR_OFFSET = 5000;
     public static final int BACKGROUND_COLOR_OFFSET = 3000;
-    public static int CURRENT_BACKGROUND = 0xFF00FF00;
+    public static int CURRENT_BACKGROUND = 0x0000000;
     public static int CURRENT_FOREGROUND = 0xFF008F47;
+    public static int BLUE = 0;
+    public static int RED = 0;
+    public static int GREEN = 255;
+
+    public static int MAX_RANGE = 255;
+
 
 }
--- app/src/main/java/edu/saulmm/letters/fragments/LetterFragment.java
+++ app/src/main/java/edu/saulmm/letters/fragments/LetterFragment.java
@@ -1,5 +1,6 @@
 package edu.saulmm.letters.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
@@ -21,6 +22,9 @@ public class LetterFragment extends Fragment {
     private int backgroundColorId;
     private int foregroundColorId;
 
+    private int red, blue, green;
+
+
     public LetterFragment(int letterResource) {
         this.letterResource = letterResource;
     }
@@ -32,13 +36,11 @@ public class LetterFragment extends Fragment {
 //        Utils.CURRENT_BACKGROUND += Utils.BACKGROUND_COLOR_OFFSET;
         Utils.CURRENT_FOREGROUND = Utils.CURRENT_BACKGROUND + Utils.STROKE_COLOR_OFFSET;
 
-        d("[DEBUG]", "edu.saulmm.letters.fragments.LetterFragment - onCreateView" +
-                "\n[Current background " + Utils.CURRENT_BACKGROUND + "]");
 
         letterView = (IntroView) rootView.findViewById(R.id.intro);
         letterView.setSvgResource(letterResource);
         letterView.startSvgAnimation();
-        letterView.setBackgroundColor(Utils.CURRENT_BACKGROUND);
+        letterView.setBackgroundColor(Color.rgb(Utils.RED, Utils.GREEN, Utils.BLUE));
         letterView.setStrokeColor((Utils.CURRENT_FOREGROUND));
 
         MainLetterActivity.addViewpagerListener(onPageChangeListener);
@@ -48,25 +50,39 @@ public class LetterFragment extends Fragment {
 
     ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
         @Override
-        public void onPageScrolled(int i, float v, int i2) {
-            d("[DEBUG]", "edu.saulmm.letters.fragments.LetterFragment - onPageScrolled" +
-                    "\n[Scrolling... i: "+i+" v: "+v+" i2: "+i2+"]");
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            String debug1 = "Blue: "+Utils.BLUE+ ", OffsetPixels: "+positionOffsetPixels+", OffsetPixels % 255: "+(positionOffsetPixels % 255);
+            String debug2 = "[onPageChangeListener position: "+position+" positionOffset: "+ (int)(positionOffset * 10)+" positionOffsetPixels: "+positionOffsetPixels;
 
-            Utils.CURRENT_BACKGROUND += (i2 / 255) + 3;
-            letterView.setBackgroundColor(Utils.CURRENT_BACKGROUND);
+//            Log.d("[DEBUG]",debug2);
+            
+            if (Utils.RED < 255)
+                Utils.RED++;
+
+            if (Utils.RED >= 255 && Utils.GREEN >= 0)
+                Utils.GREEN--;
+
+//            if (Utils.BLUE >= Utils.MAX_RANGE && Utils.RED == 0)
+//                Utils.GREEN = (positionOffsetPixels / 3) % 255;
+////
+//            if (Utils.BLUE >= Utils.MAX_RANGE -1 && Utils.GREEN >= Utils.MAX_RANGE -1)
+////                Utils.RED =  positionOffsetPixels % 255;
+////
+            d("[DEBUG]", "[positionOffsetPixels]: "+positionOffsetPixels+"| Blue: "+Utils.BLUE+ " Green: "+Utils.GREEN+" Red: "+Utils.RED);
+
+//            Utils.CURRENT_BACKGROUND += (positionOffsetPixels / 255) + 3;
+            letterView.setBackgroundColor(Color.rgb(Utils.RED, Utils.GREEN, Utils.BLUE));
 
         }
 
         @Override
         public void onPageSelected(int i) {
-            d("[DEBUG]", "edu.saulmm.letters.fragments.LetterFragment - onPageSelected" +
-                    "\n[Selected...]");
+            d("[DEBUG]","[Selected...\n]");
         }
 
         @Override
         public void onPageScrollStateChanged(int i) {
-            d("[DEBUG]", "edu.saulmm.letters.fragments.LetterFragment - onPageScrollStateChanged" +
-                    "\n[ScrollStateChanged...]");
+            d("[DEBUG]", "[ScrollStateChanged...\n]");
         }
     };
 
